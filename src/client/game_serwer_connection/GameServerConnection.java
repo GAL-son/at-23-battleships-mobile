97b2942aa081556c64da44864a54ec8dbe7dc355
ldgeyexea <piotrek269962@gmail.com>
2023-04-04 00:35:37 +0200

    client-server connection class created

---
+++ src/client/game_serwer_connection/GameServerConnection.java
@@ -0,0 +1,5 @@
+package client.game_serwer_connection;
+
+public class GameServerConnection {
+// musismy jakoś dodoac te biblioteki, normalnie to się robi z gadlem ale trutaj to niewiem jak wsumie
+}
