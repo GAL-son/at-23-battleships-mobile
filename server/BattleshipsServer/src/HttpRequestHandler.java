13a68b3f15f24983fd0ab38ccbe16889fca1740b
GAL_son <pancerek1@gmail.com>
2023-04-30 17:29:50 +0200

    BaseHttpRequest handling

--- server/BattleshipsServer/src/HttpRequestHandler.java
+++ server/BattleshipsServer/src/HttpRequestHandler.java
@@ -1,9 +1,42 @@
-import org.json.*;
+import java.net.*;
+import java.io.*;
 
 public class HttpRequestHandler {
-    JSONObject requestBody;
-    String requestPlain;
+    //private int port;
+    private ServerSocket serverSocket;
 
-    
+    HttpRequestHandler(int port) throws IOException {
+        //this.port = port;
+        this.serverSocket = new ServerSocket(port);
+    }
+
+    public Socket getClientSocket() throws IOException
+    {
+        return  serverSocket.accept();
+    }
+
+    public String getHTTPRequest( Socket clientSocket) throws IOException {        
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        String message = "";
+       
+        String s;
+        while((s = in.readLine())!= null) {
+            message += s;
+            if(s.isEmpty()){
+                break;
+            }
+        }
+
+        return message;
+    }
+
+    public void sendResponse(Socket clientSocket, String response) throws IOException {
+        OutputStream client = clientSocket.getOutputStream();
+        client.write(response.getBytes());
+
+        client.flush();
+    }  
 
 }
