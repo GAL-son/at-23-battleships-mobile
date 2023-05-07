5f9ccd295167346c2901317982693b47921441d8
GAL_son <pancerek1@gmail.com>
2023-05-07 16:32:28 +0200

    gameStateUpdate

--- server/BattleshipsServer/src/GameServer.java
+++ server/BattleshipsServer/src/GameServer.java
@@ -21,7 +21,9 @@ public class GameServer {
         public static final String LOGIN = "login";
         public static final String REGISTER = "register";    
         public static final String GET_STATS = "getStats";
-
+        public static final String SEARCH_GAME = "searchGame";
+        public static final String GAME_UPDATE = "gameUpdate";
+        public static final String GAME_SET_SHIPS = "gameSetShips";
     }
 
     private ServerState state;
@@ -29,8 +31,8 @@ public class GameServer {
 
     // Server Runtime Variables
     private LinkedList<Session> sessions;
-    //private LinkedList<Socket> openConnections;
-    //private LinkedList<Request> requestQueue;
+    private LinkedList<GameState> runningGames;
+    private LinkedList<User> playerQueue;
     
     // Server Active Members
     private HttpRequestHandler httpRequestHandler;
@@ -45,8 +47,8 @@ public class GameServer {
         this.serverPort = serverPort;
 
         sessions = new LinkedList<>();
-        //openConnections = new LinkedList<>();
-        //requestQueue = new LinkedList<>();
+        runningGames = new LinkedList<>();
+        playerQueue = new LinkedList<>();
 
         try {
             httpRequestHandler = new HttpRequestHandler(serverPort);
@@ -101,6 +103,7 @@ public class GameServer {
 
     private String respond(JSONObject body) throws IOException{
         String action = body.getString("Action");
+        int PID = Integer.parseInt(body.getString("UserID"));
 
         switch (action) {
             case Actions.REGISTER:
@@ -109,12 +112,68 @@ public class GameServer {
             case Actions.LOGIN:
                 // Check if User Exists in database and check his password
 
-                createSession(Integer.parseInt(body.getString("UserID")));
+                createSession(PID);
                 return "SESSION CEARTED";
             case Actions.GET_STATS:
                 //Fetch stats from Database
 
                 return "GET_STATS";         
+            case Actions.SEARCH_GAME:
+                boolean playerExist = false;
+                Session playerSession = null;
+                int searchID = PID;
+                for(Session session : sessions) {
+                    if(session.user.ID == searchID) { 
+                        playerExist = true;
+                        playerSession = session;
+                        break;
+                    }
+                }
+
+                if(!playerExist) {
+                    return "LOGOUT";
+                }
+
+                boolean isPlayerInQueue = false;
+                User searchUser = null;
+                for(User user : playerQueue) {
+                    if(user.ID == searchID) {
+                        isPlayerInQueue = true;
+                        searchUser = user;
+                        break;
+                    }
+                }
+
+                if(!isPlayerInQueue) {
+                    playerQueue.push(playerSession.user);
+                    return "JOINED QUEUE";
+                }
+
+                if(playerQueue.size() > 1)
+                {
+                    int foundPlayer = playerQueue.get((int)Math.random()%playerQueue.size()).ID;
+                    GameState gamestate = new GameState();
+                    gamestate.addPlayers(foundPlayer, searchID);
+                    runningGames.push(gamestate);
+                    return "PLAYER FOUND" + foundPlayer + " " + gamestate.gameID;
+                } else {
+                    return "SEARCHING FOR PLAYERS";
+                }  
+            case Actions.GAME_UPDATE:
+                int gameID = Integer.valueOf(body.getString("GameID"));
+                GameState currentGame = null;
+
+                for(GameState game : runningGames) {
+                    if(gameID == game.gameID)
+                    {
+                        currentGame = game;
+                    }
+                }
+
+                String subAction = body.getString("GameAction");
+
+                resolveGameAction(currentGame, subAction, PID);
+                
             default:
                 break;
         }
@@ -129,8 +188,21 @@ public class GameServer {
         sessions.push(s);
     }
 
-    private void removeExpiredSessions()
-    {
+    private String resolveGameAction(GameState game, String action, int PID) {
+        switch (action) {
+            case Actions.GAME_SET_SHIPS:
+                
+                game.setPlayerShips(PID, null);
+                break;
+        
+            default:
+                break;
+        }
+
+        return "";
+    }
+
+    private void removeExpiredSessions() {
         for (Session session : sessions) {
             if(session.isSessionExpired()) sessions.remove(session);
         }
--- server/BattleshipsServer/src/GameState.java
+++ server/BattleshipsServer/src/GameState.java
@@ -1,15 +1,145 @@
-public class GameState {
-    public int p1ID;
-    public int p2ID;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 
-    public int turn;
+public class GameState {
+
+    /**
+     * InnerGameState
+     */
+    public record Move(int x, int y) {}
+
+    public int gameID;
+    private static int activeGames = 0;
+
+    public int[] PID;
+    public int[] shipFields;
+
+    public boolean[][] p1Board;
+    public boolean[][] p2Board;
+
+    public int turn; // ID of active plater
     public State state;
 
-    public enum State
-    {
+    public enum State {
+        NONE,
         STARTING,
         SHIP_PLACEMENT,
         RUNNING,
         FINISHED
     }
+
+    public LinkedList<Move> moves;
+
+    GameState() {
+        gameID = GameState.activeGames++;
+        state = State.STARTING;
+
+        PID = new int[2];
+        shipFields = new int[2];
+        
+        p1Board = new boolean[10][10];
+        p2Board = new boolean[10][10];
+    }
+
+    public void addPlayers(int p1ID, int p2ID) throws IOException {
+        if(state == State.NONE) {
+            throw new NullPointerException("GameSate is not initialized");
+        }
+        if(p1ID == p2ID) {
+            throw new IOException("INVALID PLAYER ID'S");
+        }
+
+        PID[0] = p1ID;
+        PID[1] = p2ID;    
+    }
+
+    private void setStartingTurn() {
+        Random generator = new Random();
+        turn = generator.nextInt(2);
+    }
+
+    public void nextTurn()
+    {
+        turn = (turn++)%2;
+    }
+
+    public void setPlayerShips(int PID, List<Move> shipsFields)
+    {
+        state = State.SHIP_PLACEMENT;
+        if(getPlayerIndex(PID) == 0) {
+            for(Move move: shipsFields) {
+                p1Board[move.x()][move.y()] = true;
+            }
+        } else {
+            for(Move move: shipsFields) {
+                p2Board[move.x()][move.y()] = true;
+            }
+        }
+    }
+
+    public void setShipFields(int PID, int numberOfShipFields) {
+        int index = getPlayerIndex(PID);
+        shipFields[index] = numberOfShipFields;
+    }
+
+    private int getPlayerIndex(int PID)
+    {
+        int index = (PID == this.PID[0]) ? 0 : 1;
+        return index;
+    }
+
+    public void startGame()
+    {
+        state = State.RUNNING;
+        setStartingTurn();
+    }
+
+    public boolean inputMove(int PID, Move move) throws IOException
+    {
+        if(PID != this.PID[turn]) {
+            throw new IOException("ITS NOT YOUR TURN");
+        }
+        moves.push(move);
+
+        boolean moveResult = false;
+        if(getPlayerIndex(PID) == 0){
+            moveResult = p1Board[move.x()][move.y()];
+        } else {
+            moveResult = p2Board[move.x()][move.y()];
+        }
+
+        if(moveResult){
+            shipFields[(getPlayerIndex(PID)+1)%2]--;
+        }
+
+        gameUpdate();
+        return moveResult;
+    }
+
+    private void gameUpdate()
+    {
+        nextTurn();
+        if(shipFields[0] == 0 || shipFields[1] == 0) {
+            state = State.FINISHED;
+        }
+    }
+
+    public int getWinderPID()
+    {
+        if(state != State.FINISHED) return 0;
+        if(shipFields[0] == 0) return PID[1];
+        return PID[0];
+    }
+
+    
+
+    
+
+
+
+
+    
+    
 }
