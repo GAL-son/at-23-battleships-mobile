ecf8e350a8dab268b2510e110b248142e903b1ab
Paweł Hajdo <pawel.hajdo001@gmail.com>
2023-06-03 21:22:39 +0200

    created connection class to connect with server created methods post, stringToJson, playerRequestBody

---
+++ app/src/main/java/com/battleships/Connection.java
@@ -0,0 +1,45 @@
+package com.battleships;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class Connection {
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+    final OkHttpClient client = new OkHttpClient();
+
+    String post(String url, RequestBody body) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    JSONObject stringToJson(String response){
+        try {
+            return new JSONObject(response);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    RequestBody playerRequestBody(String login, String password){
+
+        return new FormBody.Builder()
+                .add("login", login)
+                .add("password",password)
+                .build();
+    }
+}
--- app/src/main/java/com/battleships/LoginActivity.java
+++ app/src/main/java/com/battleships/LoginActivity.java
@@ -6,10 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.textclassifier.TextSelection;
 import android.widget.Button;
 import android.widget.EditText;
 
-import javax.security.auth.login.LoginException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.RequestBody;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,11 +24,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         Button loginButton = findViewById(R.id.buttonLogin);
         Button cancelButton = findViewById(R.id.buttonCancelRegister);
         Button goToRegisterButton = findViewById(R.id.buttonCreateAccount);
 
+        EditText login = findViewById(R.id.editTextLogin);
         EditText password = findViewById(R.id.editTextPasswordRegister);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +36,8 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.i("TAG", "onClick: "+password.getText());
                 Log.e("passwd", String.valueOf(password.getText()));
+                login(login.getText().toString(), password.getText().toString());
+                goToMainMenu();
             }
         });
 
@@ -55,5 +64,32 @@ public class LoginActivity extends AppCompatActivity {
     private void goToRegister(){
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
+    }
+
+    private void goToMainMenu(){
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        startActivity(intent);
+    }
+
+    private void login(String login, String password){
+        Connection connection = new Connection();
+        RequestBody body = connection.playerRequestBody(login, password);
+
+        new Thread(() -> {
+            try{
+                String response = connection.post("http://10.0.2.2:8080/api/login",body);
+                JSONObject json = connection.stringToJson(response);
+                Log.i("response", response);
+                Log.i("json", String.valueOf(json));
+                if(json.has("status")){
+                    String status = json.getString("status");
+                    Log.i("status",status);
+                }
+
+            }catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
     }
 }
